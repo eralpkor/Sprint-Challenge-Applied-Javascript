e07461de84073d5110b62a3b1af4d997fe551cd4
eralpkor <eralp.ny@gmail.com>
2019-08-09 13:49:29 -0400

    added component cards

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -24,24 +24,76 @@ function card(article) {
   const card = document.createElement('div');
   card.classList.add('card');
   const headline = document.createElement('div');
+  headline.textContent = article.headline;
+
   headline.classList.add('headline');
   const author = document.createElement('div');
   author.classList.add('author');
+
   // append to card element
   card.append(headline, author);
 
   const imgContainer = document.createElement('div');
   imgContainer.classList.add('img-container');
   const images = document.createElement('img');
+  images.src = article.authorPhoto;
+  // append images to img container
   imgContainer.appendChild(images);
+
   const span = document.createElement('span');
   span.textContent = `By `;
 
+  span.textContent = article.authorName;
+
   author.append(imgContainer, span);
+  imgContainer.appendChild(images);
+
+  cardsContainer.appendChild(card);
+
+  return card;
 
 }
 
+// var result = [];
+// var classes = [];
+
 axios.get('https://lambda-times-backend.herokuapp.com/articles')
+
+  // .then(res => {
+  //   result = Object.entries(res.data.articles);
+  //   // console.log(res.data.articles)
+  //   var articles = result.map(function (val) {
+  //     return val;
+  //   });
+  //   console.log(articles)
+  //   articles.map(function(val) {
+
+  //   })
+  // })
   .then(response => {
-    console.log(response.data.articles)
+    for (const [key, value] of Object.entries(response.data.articles)) {
+      console.log(key, value);
+      value.forEach(val => {
+        card(val)
+      })
+    }
   })
+
+
+
+// .then(res => res.data.articles.json())
+
+// .then(response => {
+//   response.json();
+//   console.log(response.data.articles)
+//   // var tech = response.data.articles.map(function (tech) {
+//   //   console.log(tech.javascript)
+//   // })
+
+
+//   // var author = tech.map(function (val) {
+//   //   return val;
+//   // });
+
+//   // console.log(author)
+// })
